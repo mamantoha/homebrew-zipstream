13e0616e6929ebe1c141a2a07a07259036d092be
Anton Maminov <anton.linux@gmail.com>
2025-06-24 17:34:52 +0300

    test

--- Formula/zipstream.rb
+++ Formula/zipstream.rb
@@ -1,5 +1,5 @@
 class Zipstream < Formula
-  desc "Easily share files and directories over the network"
+  desc "Easily share files and directories over the network."
   homepage "https://github.com/mamantoha/zipstream"
   url "https://github.com/mamantoha/zipstream/archive/refs/tags/v0.23.16.tar.gz"
   sha256 "208d83379b5dd556f2cd49345dfb69a59f93a9c7940772c111a0accf8ecd1bcf"
