b0aa63f7cb7f69d360866b8236e4d93c9cafb085
Anton Maminov <anton.linux@gmail.com>
2025-06-24 17:08:48 +0300

    Add zipstream formula

---
+++ Formula/zipstream.rb
@@ -0,0 +1,19 @@
+class Zipstream < Formula
+  desc "Easily share files and directories over the network"
+  homepage "https://github.com/mamantoha/zipstream"
+  url "https://github.com/mamantoha/zipstream/archive/refs/tags/v0.23.16.tar.gz"
+  sha256 "208d83379b5dd556f2cd49345dfb69a59f93a9c7940772c111a0accf8ecd1bcf"
+  license "MIT"
+
+  depends_on "crystal"
+
+  def install
+    system "shards", "install", "--production"
+    system "shards", "build", "--release"
+    bin.install "bin/zipstream"
+  end
+
+  test do
+    system "#{bin}/zipstream", "--help"
+  end
+end
