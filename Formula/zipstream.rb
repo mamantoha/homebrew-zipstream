731ade97c434825bb2e98ac3ea4dfa1c00d6f7ca
Anton Maminov <anton.linux@gmail.com>
2025-10-20 12:07:14 +0300

    v0.25.0

--- Formula/zipstream.rb
+++ Formula/zipstream.rb
@@ -1,15 +1,15 @@
 class Zipstream < Formula
   desc "Easily share files and directories over the network"
   homepage "https://github.com/mamantoha/zipstream"
-  url "https://github.com/mamantoha/zipstream/archive/refs/tags/v0.24.2.tar.gz"
-  sha256 "1c2700f9b098f6ff7e63d9629e9cf2ac4c6639f14b4c726fee5f16b8ab619dad"
+  url "https://github.com/mamantoha/zipstream/archive/refs/tags/v0.25.0.tar.gz"
+  sha256 "ef7d01d79dce0de2794d425e6216cc24b4a5f66acd785119cd5bf0600250afb0"
   license "MIT"
 
   depends_on "crystal"
 
   def install
     system "shards", "install", "--production"
-    system "shards", "build", "--release"
+    system "shards", "build", "--release", "-Dpreview_mt", "-Dexecution_context"
     bin.install "bin/zipstream"
   end
 
